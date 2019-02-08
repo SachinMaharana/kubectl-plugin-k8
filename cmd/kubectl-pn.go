69dee4c4def741ba41d5ab6e532a91f1d3df3c9b
SachinMaharana <sachin.nicky@gmail.com>
2019-02-08 16:54:45 +0530

    commiting exprimental changes

--- cmd/kubectl-pn.go
+++ cmd/kubectl-pn.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 
 	"github.com/sachin/kubectl-plugin-k8/pkg/nodepodlist"
 	"github.com/spf13/cobra"
@@ -12,8 +14,8 @@ var showNodePods bool
 
 var rootCmd = &cobra.Command{
 	Use:   "kubectl-pn",
-	Short: "test",
-	Long:  "test",
+	Short: "Custom Plugins",
+	Long:  "https://kubernetes.io/docs/tasks/extend-kubectl/kubectl-plugins/",
 	Args:  cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := cmd.ParseFlags(args); err != nil {
@@ -31,6 +33,26 @@ var versionCmd = &cobra.Command{
 		fmt.Println("v0.1.0")
 	},
 }
+var installNodeWebCmd = &cobra.Command{
+	Use:   "node-web",
+	Short: "Install my k8s app.",
+	Long:  `https://github.com/SachinMaharana/k8-port`,
+	Run: func(cmd *cobra.Command, args []string) {
+		_, lookErr := exec.LookPath("git")
+		if lookErr != nil {
+			panic(lookErr)
+		}
+
+		cmnd := exec.Command("git", "clone", "https://github.com/SachinMaharana/k8-port.git")
+		log.Printf("Cloning and waiting for it to finish...")
+		err := cmnd.Run()
+		if err != nil {
+			log.Printf("Command finished with error: %v", err)
+			os.Exit(1)
+		}
+		fmt.Println("Deploying k8s app: node-web")
+	},
+}
 
 func init() {
 	rootCmd.Flags().BoolVarP(&showNodePods, "pods-node list", "p", false, "Set this flag to include pods-node in output")
@@ -38,6 +60,7 @@ func init() {
 
 func main() {
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(installNodeWebCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
